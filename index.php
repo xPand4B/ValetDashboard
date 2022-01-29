5ebe00a87de1423030aa736319cd7b5ac11fc573
Eric Heinzl <xpand.4beatz@gmail.com>
2022-01-29 17:37:27 +0100

    Improved styling + added reload button

--- index.php
+++ index.php
@@ -92,10 +92,8 @@
 
                     $url = 'http://' . $site . '.' . $this->tld . self::VALET_PORT;
                     $isShopware = strpos(strtolower($site), 'shopware') !== false;
-
                     $result[$trimmedPath][$site] = compact('url', 'isShopware');
                 }
-
                 array_multisort($result[$trimmedPath]);
             }
 
@@ -121,43 +119,48 @@
 
     <!-- Custom styles -->
     <style>
-        .sticky {
-            position: fixed;
-            top: 0;
-            width: 100%
-        }
+        /* Backgrounds */
+        .bg-discord-primary { background-color: #36393f; }
+        .bg-discord-secondary { background-color: #2f3136; }
+        .bg-discord-tertiary { background-color: #202225; }
 
-        /**
-         * ---------------------------------
-         * Backgrounds
-         * ---------------------------------
-         */
-        .bg-discord-primary {
-            background-color: #36393f;
+        .bg-fab-primary {
+            background-color: #5865F2;
+            fill: white;
         }
-
-        .bg-discord-secondary {
-            background-color: #2f3136;
-        }
-
-        .bg-discord-tertiary {
-            background-color: #202225;
+        .bg-fab-primary:hover {
+            background-color: white;
+            fill: #5865F2;
         }
 
         .bg-fab-default {
             background-color: #36393f;
-            fill: hsl(139,calc(1*47.3%),43.9%);;
+            fill: hsl(139,calc(1*47.3%),43.9%);
         }
         .bg-fab-default:hover {
             background-color: hsl(139,calc(1*47.3%),43.9%);
             fill: white;
         }
 
-        /**
-         * ---------------------------------
-         * Components
-         * ---------------------------------
-         */
+        .bg-fab-github {
+            background-color: black;
+            fill: white;
+        }
+        .bg-fab-github:hover {
+            background-color: white;
+            fill: black;
+        }
+
+        .bg-fab-laravel {
+            background-color: #ff2d20;
+            fill: white;
+        }
+        .bg-fab-laravel:hover {
+            background-color: white;
+            fill: #ff2d20;
+        }
+
+        /* Components */
         .card-discord {
             background-color: #2f3136;
             border-left: 4px solid #43A7FF;
@@ -172,26 +175,9 @@
             cursor: pointer;
         }
 
-        .item-shopware-admin {
-            background-color: #253758;
-            color: #bcc9dc;
-        }
-        .item-shopware-admin:hover {
-            background-color: #2A3F65;
-            cursor: pointer;
-        }
-
-        /**
-         * ---------------------------------
-         * Text
-         * ---------------------------------
-         */
-        .text-discord-header-secondary {
-            color: #b9bbbe;
-        }
-        .text-discord {
-            color: #bcc9dc;
-        }
+        /* Text */
+        .text-discord-header-secondary { color: #b9bbbe; }
+        .text-discord { color: #bcc9dc; }
     </style>
 
     <title>Valet Dashboard</title>
@@ -203,10 +189,23 @@
         <div class="bg-discord-tertiary min-h-screen w-20 shadow-lg fixed">
             <!-- FABs -->
             <div class="flex flex-col items-center mt-5 space-y-5">
+                <!-- Reload window -->
+                <div class="justify-start">
+                    <button
+                        class="p-2 bg-fab-primary text-sm font-bold tracking-wide rounded-lg focus:outline-none cursor-pointer shadow-2xl transition duration-200"
+                        onclick="location.reload()"
+                        title="Reload window"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
+                            <path d="M9 12l-4.463 4.969-4.537-4.969h3c0-4.97 4.03-9 9-9 2.395 0 4.565.942 6.179 2.468l-2.004 2.231c-1.081-1.05-2.553-1.699-4.175-1.699-3.309 0-6 2.691-6 6h3zm10.463-4.969l-4.463 4.969h3c0 3.309-2.691 6-6 6-1.623 0-3.094-.65-4.175-1.699l-2.004 2.231c1.613 1.526 3.784 2.468 6.179 2.468 4.97 0 9-4.03 9-9h3l-4.537-4.969z"/>
+                        </svg>
+                    </button>
+                </div>
+
                 <!-- Github Repo -->
                 <div>
                     <button
-                        class="p-2 bg-gray-100 hover:bg-gray-500 text-white text-sm font-bold tracking-wide rounded-full focus:outline-none cursor-pointer shadow-2xl transition duration-200"
+                        class="p-2 bg-fab-github text-white text-sm font-bold tracking-wide rounded-full focus:outline-none cursor-pointer shadow-2xl transition duration-200"
                         onclick="window.open('https://github.com/xPand4B/ValetDashboard')"
                         title="Github Repository"
                     >
@@ -219,12 +218,12 @@
                 <!-- Laravel Valet -->
                 <div>
                     <button
-                        class="p-2 bg-red-100 hover:bg-red-300 text-white text-sm font-bold tracking-wide rounded-full focus:outline-none cursor-pointer shadow-2xl transition duration-200"
+                        class="p-2 bg-fab-laravel text-white text-sm font-bold tracking-wide rounded-full focus:outline-none cursor-pointer shadow-2xl transition duration-200"
                         onclick="window.open('https://laravel.com/docs/8.x/valet')"
                         title="Laravel Valet"
                     >
                         <svg viewBox="0 -.11376601 49.74245785 51.31690859" width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-                            <path d="m49.626 11.564a.809.809 0 0 1 .028.209v10.972a.8.8 0 0 1 -.402.694l-9.209 5.302v10.509c0 .286-.152.55-.4.694l-19.223 11.066c-.044.025-.092.041-.14.058-.018.006-.035.017-.054.022a.805.805 0 0 1 -.41 0c-.022-.006-.042-.018-.063-.026-.044-.016-.09-.03-.132-.054l-19.219-11.066a.801.801 0 0 1 -.402-.694v-32.916c0-.072.01-.142.028-.21.006-.023.02-.044.028-.067.015-.042.029-.085.051-.124.015-.026.037-.047.055-.071.023-.032.044-.065.071-.093.023-.023.053-.04.079-.06.029-.024.055-.05.088-.069h.001l9.61-5.533a.802.802 0 0 1 .8 0l9.61 5.533h.002c.032.02.059.045.088.068.026.02.055.038.078.06.028.029.048.062.072.094.017.024.04.045.054.071.023.04.036.082.052.124.008.023.022.044.028.068a.809.809 0 0 1 .028.209v20.559l8.008-4.611v-10.51c0-.07.01-.141.028-.208.007-.024.02-.045.028-.068.016-.042.03-.085.052-.124.015-.026.037-.047.054-.071.024-.032.044-.065.072-.093.023-.023.052-.04.078-.06.03-.024.056-.05.088-.069h.001l9.611-5.533a.801.801 0 0 1 .8 0l9.61 5.533c.034.02.06.045.09.068.025.02.054.038.077.06.028.029.048.062.072.094.018.024.04.045.054.071.023.039.036.082.052.124.009.023.022.044.028.068zm-1.574 10.718v-9.124l-3.363 1.936-4.646 2.675v9.124l8.01-4.611zm-9.61 16.505v-9.13l-4.57 2.61-13.05 7.448v9.216zm-36.84-31.068v31.068l17.618 10.143v-9.214l-9.204-5.209-.003-.002-.004-.002c-.031-.018-.057-.044-.086-.066-.025-.02-.054-.036-.076-.058l-.002-.003c-.026-.025-.044-.056-.066-.084-.02-.027-.044-.05-.06-.078l-.001-.003c-.018-.03-.029-.066-.042-.1-.013-.03-.03-.058-.038-.09v-.001c-.01-.038-.012-.078-.016-.117-.004-.03-.012-.06-.012-.09v-21.483l-4.645-2.676-3.363-1.934zm8.81-5.994-8.007 4.609 8.005 4.609 8.006-4.61-8.006-4.608zm4.164 28.764 4.645-2.674v-20.096l-3.363 1.936-4.646 2.675v20.096zm24.667-23.325-8.006 4.609 8.006 4.609 8.005-4.61zm-.801 10.605-4.646-2.675-3.363-1.936v9.124l4.645 2.674 3.364 1.937zm-18.422 20.561 11.743-6.704 5.87-3.35-8-4.606-9.211 5.303-8.395 4.833z" fill="#ff2d20"/>
+                            <path d="m49.626 11.564a.809.809 0 0 1 .028.209v10.972a.8.8 0 0 1 -.402.694l-9.209 5.302v10.509c0 .286-.152.55-.4.694l-19.223 11.066c-.044.025-.092.041-.14.058-.018.006-.035.017-.054.022a.805.805 0 0 1 -.41 0c-.022-.006-.042-.018-.063-.026-.044-.016-.09-.03-.132-.054l-19.219-11.066a.801.801 0 0 1 -.402-.694v-32.916c0-.072.01-.142.028-.21.006-.023.02-.044.028-.067.015-.042.029-.085.051-.124.015-.026.037-.047.055-.071.023-.032.044-.065.071-.093.023-.023.053-.04.079-.06.029-.024.055-.05.088-.069h.001l9.61-5.533a.802.802 0 0 1 .8 0l9.61 5.533h.002c.032.02.059.045.088.068.026.02.055.038.078.06.028.029.048.062.072.094.017.024.04.045.054.071.023.04.036.082.052.124.008.023.022.044.028.068a.809.809 0 0 1 .028.209v20.559l8.008-4.611v-10.51c0-.07.01-.141.028-.208.007-.024.02-.045.028-.068.016-.042.03-.085.052-.124.015-.026.037-.047.054-.071.024-.032.044-.065.072-.093.023-.023.052-.04.078-.06.03-.024.056-.05.088-.069h.001l9.611-5.533a.801.801 0 0 1 .8 0l9.61 5.533c.034.02.06.045.09.068.025.02.054.038.077.06.028.029.048.062.072.094.018.024.04.045.054.071.023.039.036.082.052.124.009.023.022.044.028.068zm-1.574 10.718v-9.124l-3.363 1.936-4.646 2.675v9.124l8.01-4.611zm-9.61 16.505v-9.13l-4.57 2.61-13.05 7.448v9.216zm-36.84-31.068v31.068l17.618 10.143v-9.214l-9.204-5.209-.003-.002-.004-.002c-.031-.018-.057-.044-.086-.066-.025-.02-.054-.036-.076-.058l-.002-.003c-.026-.025-.044-.056-.066-.084-.02-.027-.044-.05-.06-.078l-.001-.003c-.018-.03-.029-.066-.042-.1-.013-.03-.03-.058-.038-.09v-.001c-.01-.038-.012-.078-.016-.117-.004-.03-.012-.06-.012-.09v-21.483l-4.645-2.676-3.363-1.934zm8.81-5.994-8.007 4.609 8.005 4.609 8.006-4.61-8.006-4.608zm4.164 28.764 4.645-2.674v-20.096l-3.363 1.936-4.646 2.675v20.096zm24.667-23.325-8.006 4.609 8.006 4.609 8.005-4.61zm-.801 10.605-4.646-2.675-3.363-1.936v9.124l4.645 2.674 3.364 1.937zm-18.422 20.561 11.743-6.704 5.87-3.35-8-4.606-9.211 5.303-8.395 4.833z"/>
                         </svg>
                     </button>
                 </div>
@@ -254,7 +253,6 @@
                         </svg>
                     </button>
                 </div>
-
             </div>
         </div>
 
