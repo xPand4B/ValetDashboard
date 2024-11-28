745e074d0991c2b2c53d14878ef1ed43342f7ef7
Eric Falsett <38140317+xPand4B@users.noreply.github.com>
2024-11-28 10:30:09 +0100

    Updated author name

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8"/>
-    <meta name="author" content="Eric Heinzl"/>
+    <meta name="author" content="Eric Falsett"/>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
 
